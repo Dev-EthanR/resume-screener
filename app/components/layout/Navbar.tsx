bce5dd9ec8b1d6776bdae4b20bb7e7f294612638
Ethan <46520558+Dev-EthanR@users.noreply.github.com>
2026-06-03 17:01:49 +1000

    redirect logo to landing page

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -5,13 +5,13 @@ const Navbar = () => {
   return (
     <div className="border-b border-border">
       <div className="page-width py-4 flex items-center justify-between">
-      <div className="flex items-center">
-        <Image src="/logo.png" alt="Resumatch Logo" width={30} height={30} />
-        <span className="ml-2 font-bold text-lg text-white">Resumatch</span>
-      </div>
-      <Link href="/signin" className="btn-outline">
-        Sign In
-      </Link>
+        <Link href="/" className="flex items-center">
+          <Image src="/logo.png" alt="Resumatch Logo" width={30} height={30} />
+          <span className="ml-2 font-bold text-lg text-white">Resumatch</span>
+        </Link>
+        <Link href="/signin" className="btn-outline">
+          Sign In
+        </Link>
       </div>
     </div>
   );
