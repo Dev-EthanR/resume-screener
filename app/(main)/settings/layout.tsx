89d6417c05ac7600ef2f201b91af2a797716b7cd
Ethan <46520558+Dev-EthanR@users.noreply.github.com>
2026-06-20 14:40:11 +1000

    get ready for production

--- app/(main)/settings/layout.tsx
+++ app/(main)/settings/layout.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, Suspense } from "react";
 import SettingsNav from "../../components/settings/SettingsNav";
 
 export default function SettingsLayout({ children }: PropsWithChildren) {
@@ -6,7 +6,9 @@ export default function SettingsLayout({ children }: PropsWithChildren) {
     <div className="flex flex-col gap-8">
       <h1 className="font-bold text-3xl text-white">Settings</h1>
       <div className="flex gap-8">
-        <SettingsNav />
+        <Suspense>
+          <SettingsNav />
+        </Suspense>
         <div className="flex-1">{children}</div>
       </div>
     </div>
